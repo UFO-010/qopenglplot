b70f5ab910cde34f9cf9315f9e818dd99c1fd351
Adorable0000 <smeliy.kirill2014@yandex.ru>
2024-11-14 13:31:20 +0300

    Removed nested sanitizer options

--- source/openglplot.cpp
+++ source/openglplot.cpp
@@ -2,28 +2,6 @@
 
 #include <QVBoxLayout>
 
-#ifndef __has_feature
-// GCC does not have __has_feature...
-#define __has_feature(feature) 0
-#endif
-
-#if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS)
-#ifdef __cplusplus
-extern "C"
-#endif
-const char *__asan_default_options() {
-  // Clang reports ODR Violation errors in mbedtls/library/certs.c.
-  // NEED TO REPORT THIS ISSUE
-  return "symbolize=1:detect_stack_use_after_return=1:external_symbolizer_path=/usr/bin/llvm-symbolizer";
-
-}
-const char *__lsan_default_options() {
-  // Clang reports ODR Violation errors in mbedtls/library/certs.c.
-  // NEED TO REPORT THIS ISSUE
-  return "verbosity=1:log_threads=1:fast_unwind_on_malloc=0";
-}
-#endif
-
 
 wgtPlot::wgtPlot(QWidget *parent) : QWidget(parent)
 {
